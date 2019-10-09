0101bb091cf4108502d403ab66e44f3dc78bd6fd
Romit Chaudhari <romitchaudhari@Romits-MacBook-Pro.local>
2019-10-08 20:09:01 -0400

    final IATA

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,32 @@ var activityList = [];
 // FUNCTIONS
 // **************************************************
 
+// IATA code function 
+
+const options = {
+    fuse_options : {
+        shouldSort: true,
+        threshold: 0.4,
+        maxPatternLength: 32,
+        keys: [{
+            name: "IATA",
+            weight: 0.6
+          },
+          {
+            name: "name",
+            weight: 0.4
+          },
+          {
+            name: "city",
+            weight: 0.2
+          }
+        ]
+      }
+  };
+  
+  AirportInput("input-departure", options)
+  AirportInput("input-arrival", options)
+
 // GET Skyscanner API
 var requestFlight = {
 	"async": true,
